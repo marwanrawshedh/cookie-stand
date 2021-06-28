b59c82e7b6bb7e2a3d6caa5225e1636cfb695c29
marwanrawshedh <marwansalem55@yahoo.com>
2021-06-28 22:15:15 +0300

    constructor

--- js/java.js
+++ js/java.js
@@ -1,215 +1,330 @@
 'use strict'
 
+// let parent = document.getElementById('resturant');
+
+// let unorderd1 = document.createElement('ul');
+// parent.appendChild(unorderd1);
+// unorderd1.textContent='seattle'
+// let Seattle={
+//     max:0,
+//     min:0,
+//     avr:6.3,
+
+// max: function(){
+
+//      for(let i=6;i<12;i++){
+//          this.max= randomAge(23,65)
+//          let clc=this.avr* this.max
+//          let list1 = document.createElement('li')
+//          unorderd1.appendChild(list1)
+//          list1.textContent= i +'am:'+ clc + 'cookies ';
+// }
+// let i=12
+// if( i=12){
+//     this.max= randomAge(23,65)
+//     let clc=this.avr* this.max;
+//     let list1 = document.createElement('li')
+//                 unorderd1.appendChild(list1)
+//                 list1.textContent= i +'pm:'+ clc + 'cookies ';
+// }
+// for(let i=1;i<7;i++){
+//     this.max= randomAge(23,65)
+// let clc=this.avr* this.max;
+// let list1 = document.createElement('li')
+//             unorderd1.appendChild(list1)
+//             list1.textContent= i +'pm:'+ clc + 'cookies ';
+// // console.log(clc);
+
+
+
+
+// }}
+// }
+
+// Seattle.max()
+
+
+// let unorderd2 = document.createElement('ul');
+// parent.appendChild(unorderd2);
+// unorderd2.textContent='tokyo'
+// let Tokyo={ 
+// max:0,
+// min:0,
+// avr:1.2, 
+
+
+//     max: function(){
+
+//         for(let i=6;i<12;i++){
+//         this.max= randomAge(3,24)
+//     let clc=this.avr* this.max
+//     let list1 = document.createElement('li')
+//    unorderd2.appendChild(list1)
+//     list1.textContent= i +'am:'+ clc + 'cookies ';}
+
+//     let i=12
+//     if( i=12){
+//         this.max= randomAge(3,24)
+//         let clc=this.avr* this.max;
+//         let list1 = document.createElement('li')
+//                     unorderd2.appendChild(list1)
+//                     list1.textContent= i +'pm:'+ clc + 'cookies ';}
+
+
+
+
+
+
+
+//                     for(let i=1;i<7;i++){
+//         this.max= randomAge(3,24)
+//     let clc=this.avr* this.max;
+// let list1 = document.createElement('li')
+//             unorderd2.appendChild(list1)
+//             list1.textContent= i +'pm:'+ clc + 'cookies ';}
+
+
+
+// }}
+// Tokyo.max()
+
+
+
+
+// let unorderd3 = document.createElement('ul');
+// parent.appendChild(unorderd3);
+// unorderd3.textContent='Dubai'
+// let Dubai={
+
+//     max:0,
+//     min:0,
+// avr:3.7,
+
+// max: function(){
+
+
+
+//     for(let i=6;i<12;i++){
+//     this.max= randomAge(11,38)  
+//     let clc=this.avr* this.max
+//     let list1 = document.createElement('li')
+//    unorderd3.appendChild(list1)
+//     list1.textContent= i +'am:'+ clc + 'cookies ';}
+
+
+// let i=12
+//     if( i=12){
+//         this.max= randomAge(11,38) 
+//         let clc=this.avr* this.max;
+//         let list1 = document.createElement('li')
+//                     unorderd3.appendChild(list1)
+//                     list1.textContent= i +'pm:'+ clc + 'cookies ';}
+
+// for(let i=1;i<7;i++){
+//     this.max= randomAge(11,38)  
+//     let clc=this.avr* this.max;
+// let list1 = document.createElement('li')
+//             unorderd3.appendChild(list1)
+//             list1.textContent= i +'pm:'+ clc + 'cookies ';}
+
+// }}
+// Dubai.max()
+
+
+
+
+// let unorderd4 = document.createElement('ul');
+// parent.appendChild(unorderd4);
+// unorderd4.textContent='paris'
+
+// let Paris={
+//     max:0,
+// min:0,
+//     avr:2.3,  
+
+//     max: function(){
+//     for(let i=6;i<12;i++){
+//          this.max= randomAge(20,38)
+//     let clc=this.avr* this.max
+//     let list1 = document.createElement('li')
+//     unorderd4.appendChild(list1)
+//         list1.textContent= i +'am:'+ clc + 'cookies ';}
+
+//     let i=12
+//     if( i=12){
+//         this.max= randomAge(20,38)
+//         let clc=this.avr* this.max;
+//         let list1 = document.createElement('li')
+//       unorderd4.appendChild(list1)
+//            list1.textContent= i +'pm:'+ clc + 'cookies ';}
+//     for(let i=1;i<7;i++){
+//         this.max= randomAge(20,38)
+
+//         let clc=this.avr* this.max;
+//         let list1 = document.createElement('li')
+//                     unorderd4.appendChild(list1)
+//                     list1.textContent= i +'pm:'+ clc + 'cookies ';}
+
+// }}
+
+// Paris.max()
+
+
+
+
+// let unorderd5 = document.createElement('ul');
+// parent.appendChild(unorderd5);
+// unorderd5.textContent='lima'
+// let Lima={
+// max:0,
+// min:0,
+// avr:4.6,
+
+//     max: function(){
+
+//     for(let i=6;i<12;i++){
+//             this.max= randomAge(2,16)
+//     let clc=this.avr* this.max
+//     let list1 = document.createElement('li')
+//     unorderd5.appendChild(list1)
+//     list1.textContent= i +'am:'+ clc + 'cookies ';}
+
+// let i=12
+//     if( i=12){
+//         this.max= randomAge(2,16)
+//         let clc=this.avr* this.max;
+//         let list1 = document.createElement('li')
+//                     unorderd5.appendChild(list1)
+//                     list1.textContent= i +'pm:'+ clc + 'cookies ';}
+//                     let arr=[]
+//                     for(let i=1;i<7;i++){
+//                         this.max= randomAge(2,16)
+//                         let clc=this.avr* this.max;
+//                         arr[i]=clc
+
+
+
+
+//                         let list1 = document.createElement('li')
+//                         unorderd5.appendChild(list1)
+//                         list1.textContent= i +'pm:'+ clc + 'cookies ';}
+//                         console.log(arr)
+
+// }}
+// Lima.max()
+// function randomAge(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);}
+// let stored = []
+
+let loc = ['seattle', 'tokyo', 'dubai', 'paris', 'lima']
+let time = [' ', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',]
 let parent = document.getElementById('resturant');
-
-let unorderd1 = document.createElement('ul');
-parent.appendChild(unorderd1);
-unorderd1.textContent='seattle'
-let Seattle={
-    max:0,
-    min:0,
-    avr:6.3,
+let table = document.createElement('table')
+parent.appendChild(table)
+console.log(parent)
+function Cookies(avr, max, min) {
+    this.avr = avr
+    this.max = max
+    this.min = min
     
-max: function(){
+
+    this.numco = function () {
+        this.num = random(min, max) * this.avr
         
-     for(let i=6;i<12;i++){
-         this.max= randomAge(23,65)
-         let clc=this.avr* this.max
-         let list1 = document.createElement('li')
-         unorderd1.appendChild(list1)
-         list1.textContent= i +'am:'+ clc + 'cookies ';
-}
-let i=12
-if( i=12){
-    this.max= randomAge(23,65)
-    let clc=this.avr* this.max;
-    let list1 = document.createElement('li')
-                unorderd1.appendChild(list1)
-                list1.textContent= i +'pm:'+ clc + 'cookies ';
-}
-for(let i=1;i<7;i++){
-    this.max= randomAge(23,65)
-let clc=this.avr* this.max;
-let list1 = document.createElement('li')
-            unorderd1.appendChild(list1)
-            list1.textContent= i +'pm:'+ clc + 'cookies ';
-// console.log(clc);
-
-
-}}
+    }
 }
 
-Seattle.max()
 
 
-let unorderd2 = document.createElement('ul');
-parent.appendChild(unorderd2);
-unorderd2.textContent='tokyo'
-let Tokyo={ 
-max:0,
-min:0,
-avr:1.2, 
-
-
-    max: function(){
-        
-        for(let i=6;i<12;i++){
-        this.max= randomAge(3,24)
-    let clc=this.avr* this.max
-    let list1 = document.createElement('li')
-   unorderd2.appendChild(list1)
-    list1.textContent= i +'am:'+ clc + 'cookies ';}
-
-    let i=12
-    if( i=12){
-        this.max= randomAge(3,24)
-        let clc=this.avr* this.max;
-        let list1 = document.createElement('li')
-                    unorderd2.appendChild(list1)
-                    list1.textContent= i +'pm:'+ clc + 'cookies ';}
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 
 
+let row = document.createElement('tr')
+table.appendChild(row)
+row.textContent = '    '
+
+for (let y = 0; y < time.length; y++) {
+
+    let el = document.createElement('th')
+    row.appendChild(el)
+    el.textContent = time[y]
+}
+
+for (let y = 0; y < 1; y++) {
+    let row = document.createElement('tr')
+    table.appendChild(row)
+    row.textContent = loc[y]
+
+    for (let i = 0; i < time.length-1; i++) {
+        let el = document.createElement('td')
+        loc[y] = new Cookies(6.3, 23, 65)
+        loc[y].numco()
+        row.appendChild(el)
+        el.textContent = (loc[y].num)
+
+
+    }
+}
+for (let y = 1; y < 2; y++) {
+    let row = document.createElement('tr')
+    table.appendChild(row)
+    row.textContent = loc[y]
+
+    for (let i = 0; i < time.length-1; i++) {
+        let el = document.createElement('td')
+        loc[y] = new Cookies(1.2, 3, 24)
+        loc[y].numco()
+        row.appendChild(el)
+        el.textContent = (loc[y].num)
+    }
+}
 
 
 
 
-                    for(let i=1;i<7;i++){
-        this.max= randomAge(3,24)
-    let clc=this.avr* this.max;
-let list1 = document.createElement('li')
-            unorderd2.appendChild(list1)
-            list1.textContent= i +'pm:'+ clc + 'cookies ';}
+for (let y = 2; y < 3; y++) {
+    let row = document.createElement('tr')
+    table.appendChild(row)
+    row.textContent = loc[y]
+
+    for (let i = 0; i < time.length-1; i++) {
+        let el = document.createElement('td')
+        loc[y] = new Cookies(3.7, 11, 38)
+        loc[y].numco()
+        row.appendChild(el)
+        el.textContent = (loc[y].num)
+    }}
+
+
+
+    for (let y = 3; y < 4; y++) {
+        let row = document.createElement('tr')
+        table.appendChild(row)
+        row.textContent = loc[y]
+
+        for (let i = 0; i < time.length-1; i++) {
+            let el = document.createElement('td')
+            loc[y] = new Cookies(2.3, 20, 38)
+            loc[y].numco()
+            row.appendChild(el)
+            el.textContent = (loc[y].num)
+        }}
+        for (let y = 4; y < 5; y++) {
+            let row = document.createElement('tr')
+            table.appendChild(row)
+            row.textContent = loc[y]
     
-
-            
-}}
-Tokyo.max()
-
-
-
-
-let unorderd3 = document.createElement('ul');
-parent.appendChild(unorderd3);
-unorderd3.textContent='Dubai'
-let Dubai={
+            for (let i = 0; i < time.length-1; i++) {
+                let el = document.createElement('td')
+                loc[y] = new Cookies(4.6, 2, 16)
+                loc[y].numco()
+                row.appendChild(el)
+                el.textContent = (loc[y].num)
+               
+               
     
-    max:0,
-    min:0,
-avr:3.7,
-    
-max: function(){
+            }}
 
-
-    
-    for(let i=6;i<12;i++){
-    this.max= randomAge(11,38)  
-    let clc=this.avr* this.max
-    let list1 = document.createElement('li')
-   unorderd3.appendChild(list1)
-    list1.textContent= i +'am:'+ clc + 'cookies ';}
-
-
-let i=12
-    if( i=12){
-        this.max= randomAge(11,38) 
-        let clc=this.avr* this.max;
-        let list1 = document.createElement('li')
-                    unorderd3.appendChild(list1)
-                    list1.textContent= i +'pm:'+ clc + 'cookies ';}
-
-for(let i=1;i<7;i++){
-    this.max= randomAge(11,38)  
-    let clc=this.avr* this.max;
-let list1 = document.createElement('li')
-            unorderd3.appendChild(list1)
-            list1.textContent= i +'pm:'+ clc + 'cookies ';}
-    
-}}
-Dubai.max()
-
-
-
-
-let unorderd4 = document.createElement('ul');
-parent.appendChild(unorderd4);
-unorderd4.textContent='paris'
-
-let Paris={
-    max:0,
-min:0,
-    avr:2.3,  
-    
-    max: function(){
-    for(let i=6;i<12;i++){
-         this.max= randomAge(20,38)
-    let clc=this.avr* this.max
-    let list1 = document.createElement('li')
-    unorderd4.appendChild(list1)
-        list1.textContent= i +'am:'+ clc + 'cookies ';}
-
-    let i=12
-    if( i=12){
-        this.max= randomAge(20,38)
-        let clc=this.avr* this.max;
-        let list1 = document.createElement('li')
-      unorderd4.appendChild(list1)
-           list1.textContent= i +'pm:'+ clc + 'cookies ';}
-    for(let i=1;i<7;i++){
-        this.max= randomAge(20,38)
-
-        let clc=this.avr* this.max;
-        let list1 = document.createElement('li')
-                    unorderd4.appendChild(list1)
-                    list1.textContent= i +'pm:'+ clc + 'cookies ';}
-
-}}
-
-Paris.max()
-
-
-
-
-let unorderd5 = document.createElement('ul');
-parent.appendChild(unorderd5);
-unorderd5.textContent='lima'
-let Lima={
-max:0,
-min:0,
-avr:4.6,
-
-    max: function(){
-
-    for(let i=6;i<12;i++){
-            this.max= randomAge(2,16)
-    let clc=this.avr* this.max
-    let list1 = document.createElement('li')
-    unorderd5.appendChild(list1)
-    list1.textContent= i +'am:'+ clc + 'cookies ';}
-
-let i=12
-    if( i=12){
-        this.max= randomAge(2,16)
-        let clc=this.avr* this.max;
-        let list1 = document.createElement('li')
-                    unorderd5.appendChild(list1)
-                    list1.textContent= i +'pm:'+ clc + 'cookies ';}
-                    let arr=[]
-                    for(let i=1;i<7;i++){
-                        this.max= randomAge(2,16)
-                        let clc=this.avr* this.max;
-                        arr[i]=clc
-                      
-
-                       
-
-                        let list1 = document.createElement('li')
-                        unorderd5.appendChild(list1)
-                        list1.textContent= i +'pm:'+ clc + 'cookies ';}
-                        console.log(arr)
-                        
-}}
-Lima.max()
-
-function randomAge(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);}
